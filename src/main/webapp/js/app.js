ccff7fa4036e0db2853c477f099f609d4429705e
Sukanta Mandal <sukanta.14319@gmail.com>
2017-08-03 15:26:56 +0530

    refactor: ng-route and templates

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,17 +1,18 @@
 var env = {};
 
 // Import variables if present (from env.js)
-if(window){  
-  Object.assign(env, window.__env);
+if (window) {
+	Object.assign(env, window.__env);
 }
 
-var userApp = angular.module('userApp', []);
+var userApp = angular.module('userApp', ['ngRoute']);
 
-//Register environment in AngularJS as constant
+// Register environment in AngularJS as constant
 userApp.constant('__env', env);
 
 var userUrl = __env.apiUrl + __env.baseUrl;
 
+//service to interact with Rest API
 userApp.factory('userService', function($http, $q) {
 
 	return {
@@ -32,10 +33,10 @@ userApp.factory('userService', function($http, $q) {
 
 			return defer.promise;
 		},
-		
-		getUserInfo : function(userId){
+
+		getUserInfo : function(userId) {
 			var defer = $q.defer();
-			var URL = userUrl+"/"+userId.id;
+			var URL = userUrl + "/" + userId.id;
 			console.log(URL)
 			$http({
 				method : 'GET',
@@ -50,10 +51,10 @@ userApp.factory('userService', function($http, $q) {
 
 			return defer.promise;
 		},
-		
-		deleteUser : function(userId){
+
+		deleteUser : function(userId) {
 			var defer = $q.defer();
-			var URL = userUrl+"/"+userId;
+			var URL = userUrl + "/" + userId;
 			console.log(URL)
 			$http({
 				method : 'DELETE',
@@ -68,16 +69,20 @@ userApp.factory('userService', function($http, $q) {
 
 			return defer.promise;
 		},
-		
-		saveUser : function($scope){
+
+		saveUser : function($scope) {
 			var defer = $q.defer();
 			var URL = userUrl;
 			console.log(URL)
 			$http({
 				method : 'POST',
 				url : userUrl,
-				headers: {'Content-Type': 'application/json'},
-			    data: $scope.model,//"{\"id\":1,  \"name\": \"Sukantaaa\",  \"email\": \"Sukanta@test.com\",  \"profession\": \"Project Lead\"}",
+				headers : {
+					'Content-Type' : 'application/json'
+				},
+				data : $scope.model,// "{\"id\":1, \"name\": \"Sukantaaa\",
+			// \"email\": \"Sukanta@test.com\",
+			// \"profession\": \"Project Lead\"}",
 			}).then(function successCallback(data, status, headers, config) {
 				defer.resolve(data);
 			},
@@ -88,16 +93,20 @@ userApp.factory('userService', function($http, $q) {
 
 			return defer.promise;
 		},
-		
-		updateUser : function($scope){
+
+		updateUser : function($scope) {
 			var defer = $q.defer();
 			var URL = userUrl;
 			console.log(URL)
 			$http({
 				method : 'PUT',
 				url : userUrl,
-				headers: {'Content-Type': 'application/json'},
-			    data: $scope.userInfo,//"{\"id\":1,  \"name\": \"Sukantaaa\",  \"email\": \"Sukanta@test.com\",  \"profession\": \"Project Lead\"}",
+				headers : {
+					'Content-Type' : 'application/json'
+				},
+				data : $scope.userInfo,// "{\"id\":1, \"name\": \"Sukantaaa\",
+			// \"email\": \"Sukanta@test.com\",
+			// \"profession\": \"Project Lead\"}",
 			}).then(function successCallback(data, status, headers, config) {
 				defer.resolve(data);
 			},
@@ -112,146 +121,128 @@ userApp.factory('userService', function($http, $q) {
 	};
 });
 
-userApp.controller('userCtrl', function($scope, userService) {
-	$scope.showTable = 0;
-	$scope.addUser = 0;
-	$scope.showSelect = 0;
-	$scope.showDelete =0;
-	$scope.showUpdate = 0
+// configure our routes
+userApp.config(function($routeProvider,$locationProvider) {
+	$routeProvider
+
+	// route for the home page
+	.when('/', {
+		templateUrl : 'templates/home.html',
+		controller : 'homeController'
+	})
 	
-	$scope.deleteUser= function(id){
-		
-		var userPromise = userService.deleteUser(id);
+	// route for the add page
+	.when('/add', {
+		templateUrl : 'templates/createUser.html',
+		controller : 'createUserController'
+	})
 
-		userPromise.then(
+	// route for the update page
+	.when('/update', {
+		templateUrl : 'templates/updateUser.html',
+		controller : 'updateUserController'
+	})
 
-		function(response) {
-			
-			$scope.userInfo = response.data;
-			window.alert("user Deleted..");
-			getAllUsers($scope, userService);
-			
-			console.log("Success : getUserById" + $scope.users)
-		}, function(error) {
-			console.log("Failed : getUserById" + error)
-		});
-	}
-	$scope.getAllUsers = function() {
-		getAllUsers($scope, userService)
-		$scope.showSelect = 0;
-		$scope.showTable = 1;
-		$scope.addUser = 0;
-		$scope.showDelete =0;
-		$scope.showUpdate = 0
-	};
+	// route for the view page
+	.when('/view', {
+		templateUrl : 'templates/allUsers.html',
+		controller : 'allUserController'
+	})
 
-	$scope.getUserId = function() {
-		getAllUsers($scope, userService);
-		$scope.showTable = 0;
-		$scope.showSelect = 1;
-		$scope.addUser = 0;
-		$scope.showDelete =0;
-		$scope.showUpdate = 0
-		$scope.userInfo={};
-	}
-	
-	$scope.getUserByID = function(id) {
-		
-		var userPromise = userService.getUserInfo(id);
-
-		userPromise.then(
-
-		function(response) {
-			
-			$scope.userInfo = response.data;
-			console.log("Success : getUserById" + $scope.users)
-		}, function(error) {
-			console.log("Failed : getUserById" + error)
-		});
-	}
-	
-	$scope.addUserView = function(){
-		$scope.showTable = 0;
-		$scope.showSelect = 0;
-		$scope.addUser = 1;
-		$scope.showDelete =0;
-		$scope.showUpdate = 0;
-		$scope.status={};
-		/*$scope.model.id="";
-		$scope.model.name="";
-		$scope.model.profession="";*/
-	}
-	
-	$scope.updateUserView = function(){
-		getAllUsers($scope, userService);
-		$scope.showTable = 0;
-		$scope.showSelect = 0;
-		$scope.addUser = 0;
-		$scope.showDelete =0;
-		$scope.showUpdate = 1
-		$scope.status={};
-		$scope.userInfo.name="";
-		$scope.userInfo.profession="";
-		$scope.userInfo.email="";
-	}
-	
-	$scope.saveUser = function(form){
-		$scope.showTable = 0;
-		$scope.showSelect = 0;
-		$scope.addUser = 1;
-		$scope.showDelete =0;
-		$scope.showUpdate = 0
-        if(form.$valid) {
-        	
-        	
-        	var userPromise = userService.saveUser($scope);
-
-    		userPromise.then(
-
-    		function(response) {
-    			
-    			$scope.status = response.data;
-    			document.getElementById('id01').style.display='block';
-    			console.log("Success : saveUser" + $scope.status)
-    		}, function(error) {
-    			console.log("Failed : saveUser" + error)
-    		});
-        	
-        	
-        } else{
-             window.alert("Invalid Input");
-        }
-    };
-    
-    $scope.updateUser = function(form){
-		$scope.showTable = 0;
-		$scope.showSelect = 0;
-		$scope.addUser = 0;
-		$scope.showDelete =0;
-		$scope.showUpdate = 1
-        if(form.$valid) {
-        	
-        	
-        	var userPromise = userService.updateUser($scope);
-
-    		userPromise.then(
-
-    		function(response) {
-    			
-    			$scope.status = response.data;
-    			console.log("Success : updateUser" + $scope.status)
-    		}, function(error) {
-    			console.log("Failed : updateUser" + error)
-    		});
-        	
-        	
-        } else{
-             window.alert("Invalid Input");
-        }
-    };
+	// route for the view page
+	.when('/viewid', {
+		templateUrl : 'templates/userById.html',
+		controller : 'userByIdController'
+	})
+	$locationProvider.hashPrefix('');
 
 });
 
+//create the controller and inject Angular's $scope
+userApp.controller('homeController', function($scope) {
+	
+});
+
+// create the controller and inject Angular's $scope
+userApp.controller('createUserController', function($scope,userService) {
+	// to save the user
+	$scope.saveUser = function(form) {
+		if (form.$valid) {
+			var userPromise = userService.saveUser($scope);
+			userPromise.then(
+			function(response) {
+				$scope.status = response.data;
+				console.log("Success : saveUser" + $scope.status)
+			}, function(error) {
+				console.log("Failed : saveUser" + error)
+			});
+		} else {
+			window.alert("Invalid Input");
+		}
+		$scope.model={};
+	};
+});
+
+userApp.controller('updateUserController', function($scope,userService) {
+	console.log("In updateUserController Controller");
+	//Get all the User Id
+	getAllUsers($scope, userService);
+	
+	//Populate the data based on ID
+	$scope.getUserByID = function(id) {
+		getUserByID($scope, userService,id)
+	}
+	
+	$scope.updateUser = function(form) {
+		if (form.$valid) {
+			var userPromise = userService.updateUser($scope);
+			userPromise.then(
+			function(response) {
+				$scope.status = response.data;
+				console.log("Success : updateUser" + $scope.status)
+				$scope.userInfo={};
+			}, function(error) {
+				console.log("Failed : updateUser" + error)
+			});
+
+		} else {
+			window.alert("Invalid Input");
+		}
+	};
+});
+
+userApp.controller('allUserController', function($scope,userService) {
+
+	//Get all the User Id
+	getAllUsers($scope, userService);
+	
+	//delete User
+	$scope.deleteUser = function(id) {
+		var userPromise = userService.deleteUser(id);
+		userPromise.then(
+		function(response) {
+			$scope.userInfo = response.data;
+			window.alert("user Deleted..");
+			getAllUsers($scope, userService);
+			console.log("Success : getUserById" + $scope.users)
+		}, function(error) {
+			console.log("Failed : getUserById" + error)
+		});
+	}
+});
+
+userApp.controller('userByIdController', function($scope,userService) {
+	
+	//Get all the User Id
+	getAllUsers($scope, userService);
+	
+	//Populate the data based on ID
+	$scope.getUserByID = function(id) {
+		getUserByID($scope, userService,id)
+	}
+});
+
+// global function used in multiple controller
 var getAllUsers = function($scope, userService) {
 	$("#spinner").show();
 	var userPromise = userService.getUsers();
@@ -267,3 +258,18 @@ var getAllUsers = function($scope, userService) {
 		console.log("Failed : getUsers" + error)
 	});
 }
+
+var getUserByID = function($scope, userService,id) {
+	var userPromise = userService.getUserInfo(id);
+
+	userPromise.then(
+
+	function(response) {
+
+		$scope.userInfo = response.data;
+		console.log("Success : getUserById" + $scope.users)
+	}, function(error) {
+		console.log("Failed : getUserById" + error)
+	});
+}
+
